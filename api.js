29ef497e1599aa2c80fec5485262b7048e17bfa7
tonysau-desktop <azn_rulz@hotmail.com>
2017-06-14 19:30:45 -0400

    testset
    teststse

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ var express    = require('express');
 var app        = express(); 
 var bodyParser = require('body-parser');
 var path = require('path');
-var monmessage ='Voici un message pour la partie 2!';
+var monmessage = "Voici un message pour la partie 2";
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -66,10 +66,11 @@ router.route("/message")
         res.json({message: monmessage});
     });
 
-router.route("/message")
+router.route("/message/messageModifie")
 //Modifier le message
     .put(function(req, res) {
         monmessage = req.body.message;
+
         res.json({message: 'Message modifié avec succès.'});
     });
 
