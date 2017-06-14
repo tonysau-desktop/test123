3919a70002d3019debd89b7c8b71950e78029139
tonysau-desktop <azn_rulz@hotmail.com>
2017-06-14 19:47:05 -0400

    remove 2
    2

--- api.js
+++ api.js
@@ -2,11 +2,10 @@ var express    = require('express');
 var app        = express(); 
 var bodyParser = require('body-parser');
 var path = require('path');
-var monmessage = "Voici un message pour Partie 2!!!";
+var monmessage = "Voici un message pour Partie 2!!";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public')); 
 
 var port = process.env.PORT || 8080;
 
@@ -35,20 +34,16 @@ router.route("/message")
 router.route("/message")
 //Modifier le message
     .put(function(req, res) {
+
         monmessage = req.body.message;
 
-        res.json({message: 'Message modifié avec succès!!!'});
+        res.json({message: 'Message modifié avec succès!!'});
     });
 
 // FIN DES ROUTES
 
 //Enregistre les routes d'API pour qu'elles soient accessibles sur /api
 app.use('/api', router);
-
-// Page Angular de présentation des messages
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
-});
 
 // Démarre le serveur
 app.listen(port);
